6abd2f00a69a0ec3b7e2d1dde1f0694081e15d4a
Gabriel Iovino <giovino@fsi.io>
2019-04-30 15:46:31 -0400

    initial commit of cli framework

---
+++ dnsdb/cli/utils.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Utility functions needed by the DNSDB module
+"""
+
+
+def epilog():
+
+    epilog_text = """
+    Time format options:
+    yyyy (2016),
+    yyyy-mm (2016-01),
+    yyyy-mm-dd (2016-01-01),
+    yyyymmdd (20160101),
+    yyyymmddThh (20160101T12),
+    yyyy-mm-ddThh:mm:ss-hh:mm (2016-01-01T00:00:00-00:00)
+    """
+    return epilog_text
+
